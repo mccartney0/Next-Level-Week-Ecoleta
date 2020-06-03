d621ef8c0e2758e68f3c10ba265da97b6a0323a2
Allan Gomes <traduvert@icloud.com>
2020-06-03 12:52:54 -0300

    Incremento da API de Estados e Cidades do IBGE

---
+++ scripts/intro.js
@@ -0,0 +1,77 @@
+// comments
+// document.write("Hello");
+
+// var myVar; //undefined
+// myVar = "Hello";
+
+// document.write(myVar);
+
+// const myConst = "He"; //Constante, não muda o valor
+
+// document.write(myConst + myVar);
+
+// let variavel = "Allan"; //let mesmo que var nos JS mais modernos
+
+//String
+// const s1 = "String"
+// const s2 = 'String'
+// const s3 = `String`
+// document.write(s1 + s2);
+
+// Number
+// const n1 = 1;
+// const n2 = 12;
+// document.write(n1 + n2);
+
+// Boolean - true ou false
+// const bTrue = true;
+// const bFalse = false;
+// document.write(bTrue);
+
+//Estrutura de dados - Objetos
+// todos objetos possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){ //funcionalidade
+//     document.write("executar uma grande logica de correr")
+//     // return "run Forest" //Para não aparecer undefined
+//   } 
+// }
+
+// // document.write(pessoa.correr());
+
+// pessoa.correr()
+
+//Array - Vetores - Coleção de dados
+// const colecaoDeBolinhas = ["blue","red", 1, {name:"Allan"}];
+
+// document.write(colecaoDeBolinhas[3].name);//Acessando o nome do objeto
+
+//Funções - Funcionalidades - Atalhos - Reuso de código
+
+// Registrar função
+// function sayMyName(name){
+//   document.write(name);
+// }
+
+// // Executando a função
+// sayMyName("Douglas<br>");
+// sayMyName("Allan");
+
+//Condicionais
+
+// const notaFinal = 7;
+
+// if(notaFinal < 5) {
+//    document.write("Reprovado");
+// }else{
+//   document.write("Aprovado")
+// }
+
+// Loop - Repetições
+
+// for(i = 0; i < 10; i++){
+//   document.write(`<p>${i}</p>`);//Ao utilizar crase, conseguimos inserir valiaveis - uma interpolação ${}
+// }
